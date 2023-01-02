4804ebb5a1f9f694caa5ac9b77247352a17e1b76
Wbbjk <121327686+Wbbjk@users.noreply.github.com>
2023-01-02 22:38:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'bPZ0VcMhqYxGnJ9ShWwGbXDlKUG_cALK6I-fFYaKziM',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o4D0F6cfeCZqtTVZ-7JB6VOtMK20',
+      id: '',
     }
   ],
 
